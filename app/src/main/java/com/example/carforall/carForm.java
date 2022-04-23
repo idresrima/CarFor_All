cf5d98be8cbdc9eba438093195fa2c86492fc594
Oday <oday.128@gmail.com>
2022-04-23 15:52:57 +0300

    not done yet 2

---
+++ app/src/main/java/com/example/carforall/carForm.java
@@ -0,0 +1,14 @@
+package com.example.carforall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class carForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_form);
+    }
+}
